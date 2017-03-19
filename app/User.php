244b8c648708e97df3c0a68fbfa04f1764c4221f
Kishan <kumar.kishan@outlook.com>
2017-03-19 13:12:53 +0545

    Added Relationships to User

--- app/User.php
+++ app/User.php
@@ -26,4 +26,23 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function social()
+    {
+        return $this->hasOne('App\Social');
+    }
+    public function companies()
+    {
+        return $this->hasMany('App\Company');
+    }
+
+    public function stories()
+    {
+        return $this->hasMany('App\Story');
+    }
+    public function rate_reviews()
+    {
+        return $this->hasMany('App\RateReview');
+    }
 }
+
