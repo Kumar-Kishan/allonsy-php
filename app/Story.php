ea3252b221e0044459866e7869a6ed988a78d401
Kishan <kumar.kishan@outlook.com>
2017-03-19 13:23:49 +0545

    Relattions to Story

--- app/Story.php
+++ app/Story.php
@@ -4,10 +4,32 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
+
 class Story extends Model
 {
     use SoftDeletes;
     protected $fillable = [
          'type',
     ];
+
+    public function review()
+    {
+        if($this->type == 0)
+        {
+            return $this->hasOne('App\Review');
+        }
+    }
+    public function post()
+    {
+        if($this->type == 1)
+        {
+            return $this->hasOne('App\Post');
+        }
+    }
+
+    public function rate_reviews()
+    {
+        return $this->hasMany('App\RateReview');
+    }
 }
