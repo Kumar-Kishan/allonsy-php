d1b22e6bc7def8ff1386d7d2c1c478bbd63c4e96
JJ <shresthayaju@gmail.com>
2017-03-19 14:57:10 +0545

    Factory Made

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,42 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Company::class, function (Faker\Generator $faker) {
+    
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->companyEmail,
+        'contact_no' => $faker->phoneNumber,
+
+        'location_id' => $faker->numberBetween(0,1000),
+        'about' => $faker->realText($maxNbChars = 200, $indexSize = 2),
+        'website' => $faker->nullable()->url,
+    ];
+});
+
+$factory->define(App\Location::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'lat' => $faker->latitude($min = -90, $max = 90),
+        'long' => $faker->longitude($min = -180, $max = 180),
+        'geo_address' => $faker->address,
+    ];
+});
+
+$factory->define(App\Media::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'type' => $faker->numberBetween(0,1),
+    ];
+});
+
+$factory->define(App\Media::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'type' => $faker->numberBetween(0,1),
+    ];
+});
