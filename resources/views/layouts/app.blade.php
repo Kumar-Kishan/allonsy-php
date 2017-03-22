daa31d9a0fcc69fd97bbd8ee9d20fc7ad431c619
Kishan <kumar.kishan@outlook.com>
2017-03-22 19:29:25 +0545

    removed cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,9 +55,8 @@
             @yield('content')
         </div>
     <div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
-    @yield('scripts')
+    <script src="{{ url('js/app.js') }}"></script>
+       @yield('scripts')
 </body>
     
 </html>
