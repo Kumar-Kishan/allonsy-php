a47896dd0096b5f3c1217cc2f1b21cae2630cb2c
Nikita Dulal <Nikita Dulal>
2017-06-10 12:20:20 +0545

    aru changes

--- resources/views/feed/index.blade.php
+++ resources/views/feed/index.blade.php
@@ -2,31 +2,16 @@
 
 @section('styles')
 <style>
-    .sidebar {
-    position: absolute;
-    width: 20%;
-    height: 100%;
-    z-index: 10001;
-    visibility: block;
-}
+   
 </style>
 @endsection
 
 @section('content')
-    
-<div class="main-container">
 
-  <div class="col-lg-3  sidebar sidebar-left sidebar-lg-show active" id="leftsidebar">
-        <profile-bar></profile-bar>
-  </div>
-
-  <div class="col-lg-6 col-md-12 col-sm-offset-3 main-content">
-    <p>mid content</p>
-  </div>
-
-  <div  class=" col-lg-3 sidebar sidebar-right sidebar-lg-show active">
-    <p>Suggested places</p>
-  </div>
+<div class="panel panel-default">
+    <div class="panel-body">
+            <profile-bar></profile-bar>
+    </div>
 </div>
     
 
