04c4326e82ecfcd176845fa9743f64eac57eaeae
Nikita Dulal <Nikita Dulal>
2017-07-22 16:31:17 +0545

    landing page modify niki

--- resources/views/landinglayout/content.blade.php
+++ resources/views/landinglayout/content.blade.php
@@ -65,40 +65,26 @@
 						<div class="box alt">
 							<div class="row uniform">
 								<section class="4u 6u(medium) 12u$(xsmall)">
-									<span class="icon alt major fa-area-chart"></span>
-									<h3>Ipsum sed commodo</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+									<span class="icon alt major fa-area-chart"><img src="images/user.jpg" alt="" /></span>
+									<h3>User Profile</h3>
+									<p>Set up your profile and preferences and let the journey begin.</p>
 								</section>
 								<section class="4u 6u$(medium) 12u$(xsmall)">
-									<span class="icon alt major fa-comment"></span>
-									<h3>Eleifend lorem ornare</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+									<span class="icon alt major fa-comment"><img src="images/review.jpg" alt="" /></span>
+									<h3>Review</h3>
+									<p>Give reviews about different places and let the world know about the amazing wonders of planet Earth.</p>
 								</section>
 								<section class="4u$ 6u(medium) 12u$(xsmall)">
-									<span class="icon alt major fa-flask"></span>
-									<h3>Cubilia cep lobortis</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
+									<span class="icon alt major fa-flask"><img src="images/recommendation.jpg" alt="" /></span>
+									<h3>Recommendation</h3>
+									<p>Don't think too much about where to go. Set up your preferences and we'll recommend you perfect places as per your interests.</p>
 								</section>
-								<section class="4u 6u$(medium) 12u$(xsmall)">
-									<span class="icon alt major fa-paper-plane"></span>
-									<h3>Non semper interdum</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
-								<section class="4u 6u(medium) 12u$(xsmall)">
-									<span class="icon alt major fa-file"></span>
-									<h3>Odio laoreet accumsan</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
-								<section class="4u$ 6u$(medium) 12u$(xsmall)">
-									<span class="icon alt major fa-lock"></span>
-									<h3>Massa arcu accumsan</h3>
-									<p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
-								</section>
+								               							
 							</div>
 						</div>
 						<footer class="major">
 							<ul class="actions">
-								<li><a href="#" class="button">Magna sed feugiat</a></li>
+								<li><a href="{{ route('register') }}"class="button">SIGN UP NOW!</a></li>
 							</ul>
 						</footer>
 					</div>
